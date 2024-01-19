ccfbf6cf4aaec180bfcd3446b213d54efdd2c635
Jeff Sagal <sagalbot@gmail.com>
2024-01-19 14:36:10 -0800

    Initial commit

---
+++ components/AutoImport.story.vue
@@ -0,0 +1,19 @@
+<script setup>
+/* eslint-disable no-undef */
+console.log('useNuxtApp', useNuxtApp())
+console.log('useNuxtApp().$config', useNuxtApp().$config)
+const config = useRuntimeConfig()
+console.log('useRuntimeConfig', config)
+</script>
+
+<template>
+  <Story>
+    <Meow />
+
+    <h3>Nuxt runtime config</h3>
+    <pre>{{ config }}</pre>
+    <p data-testid="config">
+      {{ config.public.configFromNuxt }}
+    </p>
+  </Story>
+</template>
